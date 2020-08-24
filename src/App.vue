1ae38d7deaa26396794c51c135dac14f7804fb3a
Tame <tame@Eyoatams-MacBook-Pro.local>
2020-08-24 09:53:53 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+} */
+
+body {
+  background-color: #e9ecef;
+  font-family: "Montserrat", sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  /* align-items: center; */
+  padding-top: 50px;
+}
+
+body,
+html {
+  margin: 0;
+  height: 100%;
+}
+
+#app {
+  width: 50%;
+}
+
+/* #nav {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+} */
+
+#nav {
+  padding: 20px 20px 20px 0;
+}
+
+#nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 225);
+  font-weight: bold;
+  margin-right: 15px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Skills />
+  </div>
+</template>
+
+<script>
+import Skills from "../components/Skills.vue";
+
+export default {
+  name: "App",
+  components: {
+    Skills,
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap");
+
+body {
+  background-color: #e9ecef;
+  font-family: "Montserrat", sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+
+body,
+html {
+  margin: 0;
+  height: 100%;
+}
+
+#app {
+  width: 50%;
+}
+</style>
